17b39253474598a7206e6da3a2d973f95f7eecbd
ChhunlinOn <chhunlin.on@institute.pse.ngo>
2024-06-06 10:47:47 +0700

    done dom1

--- dom1/index.js
+++ dom1/index.js
@@ -2,3 +2,13 @@ const article = {
   title: "How to code",
   description: "You can learn to code. Error is nothing to be afraid of.",
 };
+let display = document.getElementById('article');
+
+document.querySelector('button').addEventListener('click',show);
+
+function show(){
+  display.innerHTML = `
+  <p>${article.title}</p>
+  <p>${article.description}</p>
+  `
+}
